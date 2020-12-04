bce07234579d11652e26caa0eb2dce156a0adc90
Marchenko Yaroslava <fortunefame1710@gmail.com>
2020-12-04 03:33:18 +0200

    Добавила imagemin и express

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const express = require('express');
+const { message } = require('statuses');
+// const app = require('express')();
 const validator = require('validator');
-const throttle = require('lodash.throttle');
+// const throttle = require('lodash.throttle');
 const greeter = require('./greeter');
 
 greeter('Hello friends!');
@@ -18,3 +21,15 @@ console.log(
   validateEmail('Mangozedog.com'),
 );
 
+const app = express();
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World!!!<h1/>')
+// });
+app.get('/', (req, res) => {
+    res.send({
+        message: "Node.js"
+    })
+})
+app.listen(3000, () => {
+    console.log('App is running on port 3000');
+})
